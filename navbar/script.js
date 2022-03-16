698ed0fdb2eabbca550dbc237e1e173364d8f11a
Nez Mustafa <nezmustafa123@gmail.com>
2022-03-16 22:01:58 +0000

    transition

--- navbar/script.js
+++ navbar/script.js
@@ -8,5 +8,23 @@
   function addClass(element, className) {
     let classList = element.className.split("");
     classList.push(className);
+    element.className = classList.join("");
+  }
+  function removeClass(element, className) {
+    let classList = element.className.split("");
+    classList = classList.filter(function (e) {
+      return e !== className;
+    });
+    element.classList = classList;
+  }
+  function toggleClass(element, className) {
+    let classList = element.className.split("");
+    if (classList.indexOf(className) !== -1) {
+      console.log("true");
+      removeClass(element, className);
+    } else {
+      console.log("false");
+      addClass(element, className);
+    }
   }
 });
